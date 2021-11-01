e83084f8e6ffc5e84a72015f1e6454e30370ae88
Anuj-kiet <53224310+Anuj-kiet@users.noreply.github.com>
2021-11-01 12:44:53 +0530

    Add files via upload

---
+++ Que2.java
@@ -0,0 +1,43 @@
+package CoreJava;
+
+class Employee{
+    float salary = 40000;
+    void totalSalary()
+    {
+        System.out.println("The Employee incremented salary is :" +(salary ) );
+
+    }
+}
+class Manager extends Employee{
+    float incentive =15000;
+    void totalsalary()
+    {
+        System.out.println("The Manager salary is :" +(salary+incentive));
+    }
+}
+class Labour extends Employee{
+    double overtime=6500;
+    void totalsalary()
+    {
+        System.out.println("The salary of the Labours is :"+(overtime+salary));
+
+
+    }
+}
+public class Que2
+{
+    public static void main(String args[]){
+
+
+        Labour l = new Labour();
+        Manager m=new Manager();
+        System.out.println("Total Salary Of Employees In Organisation is :"+(m.salary+ ((m.incentive)+ (l.overtime))+ l.salary)); //displays total salary of emp in org//
+        
+
+        // based on an object it decides which class salary method to be execute
+       m.totalsalary(); // displays manager salary//
+       l.totalsalary(); //displays labour salary//
+
+
+    }
+}
---
+++ Que3.java
@@ -0,0 +1,42 @@
+package CoreJava;
+
+class Bank{
+
+    void totalMoneyInBank()
+    {
+        System.out.println("The Employee incremented salary is :" );
+
+    }
+}
+class SavingAcoount extends Bank{
+    float fixedDeposits =150000;
+    void totalMoneyInBank()
+    {
+        System.out.println("total money in savings account :" +(fixedDeposits));
+    }
+}
+class CurrentAcccount extends SavingAcoount{
+    double cashCredit=59000;
+    void totalMoneyInBank()
+    {
+        System.out.println("The total money in current account :"+(cashCredit));
+
+
+    }
+}
+public class Que3
+{
+    public static void main(String args[]){
+
+        CurrentAcccount c=new CurrentAcccount();
+        SavingAcoount s=new SavingAcoount();
+
+
+        // based on an object it decides which class account method to be execute
+        c.totalMoneyInBank(); //prints total money in currect ac//
+        s.totalMoneyInBank(); //prints total money in savings ac//
+
+        System.out.println("Total money in the bank  is :"+( c.fixedDeposits+c.cashCredit)); //displays total salary of emp in org//
+
+    }
+}
